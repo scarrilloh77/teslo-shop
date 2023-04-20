f910c6294708fe1ae1a192de3ab390fe1602e0b4
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-04-19 22:44:33 -0500

    Folder structure & ShopLayout

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from 'react';
+import Head from 'next/head';
+
+interface Props {
+  children: ReactNode;
+  title: string;
+  pageDescription?: string;
+  imageFullUrl?: string;
+}
+
+export const ShopLayout = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}: Props) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={pageDescription} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={pageDescription} />
+        {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+      </Head>
+      <nav></nav>
+      <main
+        style={{ margin: '80px auto', maxWidth: '1440px', padding: '0px 30px' }}
+      >
+        {children}
+      </main>
+      <footer></footer>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+import { Typography } from '@mui/material';
+import { ShopLayout } from '@/components/layouts';
+
 export default function Home() {
-  return <h1>Teslo-shop</h1>;
+  return (
+    <ShopLayout
+      title={'Teslo-Shop - Home'}
+      pageDescription={'Encuentra los mejores productos de Teslo aquí'}
+    >
+      <Typography variant="h1" component="h1">
+        Tienda
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
+  );
 }
