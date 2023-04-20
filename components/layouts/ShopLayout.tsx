ac7047feaca0a7263039ea0a8dfe0c6b8792e3aa
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-04-20 00:24:44 -0500

    SideMenu

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import Head from 'next/head';
-import { Navbar } from '../ui';
+import { Navbar, SideMenu } from '../ui';
 
 interface Props {
   children: ReactNode;
@@ -27,6 +27,7 @@ export const ShopLayout = ({
       <nav>
         <Navbar />
       </nav>
+      <SideMenu />
       <main
         style={{ margin: '80px auto', maxWidth: '1440px', padding: '0px 30px' }}
       >
