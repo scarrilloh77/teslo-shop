2a44149df1efb7bf71fa61a4e406100fb322a5d1
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-04-23 02:35:56 -0500

    Show products of shopping car

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,5 +1,14 @@
 import { initialData } from '@/database/products';
-import { Typography } from '@mui/material';
+import {
+  Button,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Link,
+  Typography,
+} from '@mui/material';
+import NextLink from 'next/link';
+import { ItemCounter } from '../ui';
 
 const productsInCart = [
   initialData.products[0],
@@ -11,7 +20,40 @@ export const CartList = () => {
   return (
     <>
       {productsInCart.map((product) => (
-        <Typography key={product.slug}>{product.slug}</Typography>
+        <Grid container spacing={2} key={product.slug} sx={{ mb: 1 }}>
+          <Grid item xs={3}>
+            <NextLink href="/" passHref legacyBehavior>
+              <Link>
+                <CardActionArea>
+                  <CardMedia
+                    image={`products/${product.images[0]}`}
+                    component="img"
+                    sx={{ borderRadius: '5px' }}
+                  ></CardMedia>
+                </CardActionArea>
+              </Link>
+            </NextLink>
+          </Grid>
+          <Grid item xs={7}>
+            <Typography variant="body1">{product.title}</Typography>
+            <Typography variant="body1">
+              Talla: <strong>M</strong>
+            </Typography>
+            <ItemCounter />
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="subtitle1">${product.price}</Typography>
+            <Button variant="text" color="secondary">
+              Remover
+            </Button>
+          </Grid>
+        </Grid>
       ))}
     </>
   );
