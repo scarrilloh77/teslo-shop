c7329f9ce318bd815dc875980a7daa39737382e3
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-04-23 02:11:06 -0500

    Initial shopping cart page

---
+++ pages/cart/index.tsx
@@ -0,0 +1,41 @@
+import { CartList } from '@/components/cart';
+import { ShopLayout } from '@/components/layouts';
+import {
+  Box,
+  Button,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+} from '@mui/material';
+
+const CartPage = () => {
+  return (
+    <ShopLayout
+      title="Carrito - 3"
+      pageDescription="Carrito de compras de la tienda"
+    >
+      <Typography variant="h1" component="h1">
+        Carrito
+      </Typography>
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <CartList />
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <CardContent>
+            <Typography variant="h2">Orden</Typography>
+            <Divider sx={{ my: 1 }} />
+            <Box sx={{ mt: 3 }}>
+              <Button color="secondary" className="circular-btn" fullWidth>
+                Checkout
+              </Button>
+            </Box>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default CartPage;
