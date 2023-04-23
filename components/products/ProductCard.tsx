e7bb0a2464e98bd44d256a9b3ad5ec941265171a
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-04-22 21:30:38 -0500

    Product card ended

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,22 +1,49 @@
 import { IProduct } from '@/interfaces';
-import { Grid, Card, CardActionArea, CardMedia } from '@mui/material';
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Box,
+  Typography,
+} from '@mui/material';
+import { useMemo, useState } from 'react';
 
 interface Props {
   product: IProduct;
 }
 
 export const ProductCard = ({ product }: Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const productImage = useMemo(() => {
+    return isHovered
+      ? `products/${product.images[1]}`
+      : `products/${product.images[0]}`;
+  }, [isHovered, product.images]);
+
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <Card>
         <CardActionArea>
           <CardMedia
             component="img"
-            image={`products/${product.images[0]}`}
+            className="fadeIn"
+            image={productImage}
             alt={product.title}
           />
         </CardActionArea>
       </Card>
+      <Box sx={{ mt: 1 }} className="fadeIn">
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>${product.price}</Typography>
+      </Box>
     </Grid>
   );
 };
