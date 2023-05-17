ee04f303e2e110a7426d44fb4f5c552da9ff637c
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-05-17 17:57:57 -0500

    Products page for men, women and children

---
+++ pages/category/men.tsx
@@ -0,0 +1,27 @@
+import { Typography } from '@mui/material';
+import { ShopLayout } from '@/components/layouts';
+import { ProductList } from '@/components/products';
+import { useProducts } from '@/hooks';
+import { FullScreenLoading } from '@/components/ui';
+
+const MenPage = () => {
+  const { products, isLoading } = useProducts('/products?gender=men');
+
+  return (
+    <ShopLayout
+      title={'Teslo-Shop - Men'}
+      pageDescription={'Encuentra los mejores productos de Teslo para hombres'}
+    >
+      <Typography variant="h1" component="h1">
+        Hombres
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Productos para hombres
+      </Typography>
+
+      {isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
+    </ShopLayout>
+  );
+};
+
+export default MenPage;
---
+++ pages/category/women.tsx
@@ -0,0 +1,27 @@
+import { Typography } from '@mui/material';
+import { ShopLayout } from '@/components/layouts';
+import { ProductList } from '@/components/products';
+import { useProducts } from '@/hooks';
+import { FullScreenLoading } from '@/components/ui';
+
+const WomenPage = () => {
+  const { products, isLoading } = useProducts('/products?gender=women');
+
+  return (
+    <ShopLayout
+      title={'Teslo-Shop - Women'}
+      pageDescription={'Encuentra los mejores productos de Teslo para mujeres'}
+    >
+      <Typography variant="h1" component="h1">
+        Mujeres
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Productos para mujeres
+      </Typography>
+
+      {isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
+    </ShopLayout>
+  );
+};
+
+export default WomenPage;
