8a2344b96fab4a3f17ecfa9fe5748f0216fb6744
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-05-17 22:46:27 -0500

    UI Context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { SWRConfig } from 'swr';
 import { lightTheme } from '@/themes';
+import { UiProvider } from '@/context';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,10 +13,12 @@ export default function App({ Component, pageProps }: AppProps) {
           fetch(resource, init).then((res) => res.json()),
       }}
     >
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <UiProvider>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UiProvider>
     </SWRConfig>
   );
 }
