354178f3f21ff1bfb42e290eb1203c6dc1eeac9e
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-05-23 01:04:18 -0500

    Calculate amounts to pay

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -39,6 +39,24 @@ export const CartProvider: FC<Props> = ({ children }) => {
     Cookie.set('cart', JSON.stringify(state.cart));
   }, [state.cart]);
 
+  useEffect(() => {
+    const numberOfItems = state.cart.reduce(
+      (prev, current) => current.quantity + prev,
+      0
+    );
+    const subTotal = state.cart.reduce(
+      (prev, current) => current.price * current.quantity + prev,
+      0
+    );
+    const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+    const orderSummary = {
+      numberOfItems,
+      subTotal,
+      tax: subTotal * taxRate,
+      total: subTotal * (taxRate + 1),
+    };
+  }, [state.cart]);
+
   const addProductToCart = (product: ICartProduct) => {
     const productInCart = state.cart.some((p) => p._id === product._id);
     if (!productInCart)
