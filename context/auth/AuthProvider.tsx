d0f5b25b854f32afbd388a24aa15b7a222f456f3
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-05-23 22:58:23 -0500

    Dispatch register actions

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -3,6 +3,7 @@ import { IUser } from '@/interfaces';
 import { AuthContext, authReducer } from './';
 import { tesloApi } from '@/api';
 import Cookies from 'js-cookie';
+import axios from 'axios';
 
 export interface AuthState {
   isLoggedIn: boolean;
@@ -32,8 +33,39 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
+  const registerUser = async (
+    name: string,
+    email: string,
+    password: string
+  ): Promise<{ hasError: boolean; message?: string }> => {
+    try {
+      const { data } = await tesloApi.post('/user/register', {
+        name,
+        email,
+        password,
+      });
+      const { token, user } = data;
+      Cookies.set('token', token);
+      dispatch({ type: '[Auth] - Login', payload: user });
+      return {
+        hasError: false,
+      };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return {
+          hasError: true,
+          message: error.response?.data.message,
+        };
+      }
+      return {
+        hasError: true,
+        message: 'No se puedo crear el usuario - intente de nuevo',
+      };
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ ...state, loginUser }}>
+    <AuthContext.Provider value={{ ...state, loginUser, registerUser }}>
       {children}
     </AuthContext.Provider>
   );
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import {
   Box,
@@ -14,6 +15,7 @@ import { AuthLayout } from '@/components/layouts';
 import { validations } from '@/utils';
 import { tesloApi } from '@/api';
 import { ErrorOutline } from '@mui/icons-material';
+import { AuthContext } from '@/context';
 
 type FormData = {
   name: string;
@@ -22,30 +24,30 @@ type FormData = {
 };
 
 const RegisterPage = () => {
+  const router = useRouter();
+  const { registerUser } = useContext(AuthContext);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
   const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const onRegisterForm = async ({ email, password, name }: FormData) => {
     setShowError(false);
-    try {
-      const { data } = await tesloApi.post('/user/register', {
-        name,
-        email,
-        password,
-      });
-      const { token, user } = data;
-      console.log({ token, user });
-    } catch (error) {
-      console.log('Error en las credenciales');
+    const { hasError, message } = await registerUser(name, email, password);
+
+    if (hasError) {
       setShowError(true);
+      setErrorMessage(message!);
       setTimeout(() => {
         setShowError(false);
       }, 3000);
+      return;
     }
+
+    router.replace('/');
   };
 
   return (
