0f1836224b74e922e5c1a714d4047065720d3ffe
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-05-25 23:41:25 -0500

    Provider - Credentials

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,39 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import Credentials from 'next-auth/providers/credentials';
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: {
+          label: 'Correo:',
+          type: 'email',
+          placeholder: 'correo@google.com',
+        },
+        password: {
+          label: 'Contraseña:',
+          type: 'password',
+          placeholder: 'Contraseña',
+        },
+      },
+      async authorize(credentials) {
+        console.log('credenciales', credentials);
+        return null;
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // ...add more providers here
   ],
+
+  // Callbacks
+  // Por defecto, NextAuth.js maneja los callbacks con jwt.
+  // Ahora NextAuth.js para requiere de la variable de entorno NEXTAUTH_SECRET para firmar los tokens.
+  callbacks: {},
 };
 
 export default NextAuth(authOptions);
