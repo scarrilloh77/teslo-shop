3dcdf748720c08c98fdd3937762ef2d25f5bb9c0
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-05-27 08:56:45 -0500

    Checkout page middleware with NextAuth

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,35 @@
 import { NextResponse, type NextRequest } from 'next/server';
 import * as jose from 'jose';
+import { getToken } from 'next-auth/jwt';
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
+  // if (req.nextUrl.pathname.startsWith('/checkout')) {
+  //   const token = req.cookies.get('token')?.value || '';
+  //   try {
+  //     await jose.jwtVerify(
+  //       token,
+  //       new TextEncoder().encode(process.env.JWT_SECRET_SEED)
+  //     );
+  //     return NextResponse.next();
+  //   } catch (error) {
+  //     return NextResponse.redirect(
+  //       new URL(`/auth/login?p=${req.nextUrl.pathname}`, req.url)
+  //     );
+  //   }
+  // }
   if (req.nextUrl.pathname.startsWith('/checkout')) {
-    const token = req.cookies.get('token')?.value || '';
-    try {
-      await jose.jwtVerify(
-        token,
-        new TextEncoder().encode(process.env.JWT_SECRET_SEED)
-      );
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect(
-        new URL(`/auth/login?p=${req.nextUrl.pathname}`, req.url)
-      );
+    const session = await getToken({ req, secret: process.env.NEXTAUH_SECRET });
+
+    if (!session) {
+      const requestedPage = req.nextUrl.pathname;
+      const url = req.nextUrl.clone();
+      url.pathname = `/auth/login`;
+      url.search = `p=${requestedPage}`;
+      return NextResponse.redirect(url);
     }
+
+    return NextResponse.next();
   }
 }
 
