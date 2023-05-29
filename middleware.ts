78746342c61c3fa1c6e1526a61d8964c5bd286c5
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-05-29 02:25:53 -0500

    Auth middlewares

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,7 @@
 import { NextResponse, type NextRequest } from 'next/server';
-import * as jose from 'jose';
 import { getToken } from 'next-auth/jwt';
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
-  // if (req.nextUrl.pathname.startsWith('/checkout')) {
-  //   const token = req.cookies.get('token')?.value || '';
-  //   try {
-  //     await jose.jwtVerify(
-  //       token,
-  //       new TextEncoder().encode(process.env.JWT_SECRET_SEED)
-  //     );
-  //     return NextResponse.next();
-  //   } catch (error) {
-  //     return NextResponse.redirect(
-  //       new URL(`/auth/login?p=${req.nextUrl.pathname}`, req.url)
-  //     );
-  //   }
-  // }
   if (req.nextUrl.pathname.startsWith('/checkout')) {
     const session = await getToken({ req, secret: process.env.NEXTAUH_SECRET });
 
@@ -31,6 +15,56 @@ export async function middleware(req: NextRequest) {
 
     return NextResponse.next();
   }
+  if (
+    req.nextUrl.pathname.startsWith('/admin') ||
+    req.nextUrl.pathname.includes('/api/admin')
+  ) {
+    const session: any = await getToken({
+      req,
+      secret: process.env.NEXTAUH_SECRET,
+    });
+
+    const requestedPage = req.nextUrl.pathname;
+
+    if (!session) {
+      const url = req.nextUrl.clone();
+      url.pathname = `/auth/login`;
+      url.search = `p=${requestedPage}`;
+
+      if (requestedPage.includes('/api')) {
+        return new Response(JSON.stringify({ message: 'No autorizado' }), {
+          status: 401,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      }
+
+      return NextResponse.redirect(url);
+    }
+
+    const validRoles = ['admin', 'super-user', 'SEO'];
+
+    if (
+      !validRoles.includes(session.user.role) &&
+      req.nextUrl.pathname.includes('/api/admin')
+    ) {
+      return new Response(JSON.stringify({ message: 'No autorizado' }), {
+        status: 401,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    if (!validRoles.includes(session.user.role)) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+
+    return NextResponse.next();
+  }
 }
 
-// El _middleware se ejecuta antes de servir las pages que se encuentran en el MISMO DIRECTORIO.
+export const config = {
+  matcher: ['/checkout/:path*', '/admin/:path*', '/api/admin/:path*'],
+};
