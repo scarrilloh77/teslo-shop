7551b77bbe7a3ad22227dc174d97ffbc2d788691
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-05-29 01:03:15 -0500

    Rest - Stadistic info

---
+++ pages/api/admin/dashboard.ts
@@ -0,0 +1,48 @@
+import { db } from '@/database';
+import { Order, Product, User } from '@/models';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  numberOfOrders: number;
+  paidOrders: number;
+  notPaidOrders: number;
+  numberOfClients: number;
+  numberOfProducts: number;
+  productsWithNoInventory: number;
+  lowInventory: number;
+};
+
+export default async function (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await db.connect();
+
+  const [
+    numberOfOrders,
+    paidOrders,
+    numberOfClients,
+    numberOfProducts,
+    productsWithNoInventory,
+    lowInventory,
+  ] = await Promise.all([
+    Order.count(),
+    Order.find({ isPaid: true }).count(),
+    User.find({ role: 'client' }).count(),
+    Product.count(),
+    Product.find({ inStock: 0 }).count(),
+    Product.find({ inStock: { $lt: 10 } }).count(),
+  ]);
+
+  await db.disconnect();
+
+  res.status(200).json({
+    numberOfOrders,
+    paidOrders,
+    numberOfClients,
+    numberOfProducts,
+    productsWithNoInventory,
+    lowInventory,
+    notPaidOrders: numberOfOrders - paidOrders,
+  });
+}
