c8fb0bc682b937556ab34c6e4859887130e64694
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-05-29 23:53:47 -0500

    Show all orders

--- pages/admin/orders.tsx
+++ pages/admin/orders.tsx
@@ -1,15 +1,86 @@
 import React from 'react';
-import { AdminLayout } from '@/components/layouts';
+import { Chip, Grid } from '@mui/material';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import { ConfirmationNumberOutlined } from '@mui/icons-material';
+import { AdminLayout } from '@/components/layouts';
+import useSWR from 'swr';
+import { IOrder, IUser } from '@/interfaces';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'Orden ID', width: 250 },
+  { field: 'email', headerName: 'Correo', width: 250 },
+  { field: 'name', headerName: 'Nombre completo', width: 300 },
+  { field: 'total', headerName: 'Monto total', width: 300 },
+  {
+    field: 'isPaid',
+    headerName: 'Pagada',
+    //@ts-ignore
+    renderCell: ({ row }: GridValueGetterParams) => {
+      return row.isPaid ? (
+        <Chip variant="outlined" label="Pagada" color="success" />
+      ) : (
+        <Chip variant="outlined" label="Pendiente" color="error" />
+      );
+    },
+  },
+  {
+    field: 'noProducts',
+    headerName: 'No.Productos',
+    align: 'center',
+    width: 150,
+  },
+  {
+    field: 'check',
+    headerName: 'Ver orden',
+    //@ts-ignore
+    renderCell: ({ row }: GridValueGetterParams) => {
+      return (
+        <a href={`/admin/orders/${row.id}`} target="_blank">
+          Ver orden
+        </a>
+      );
+    },
+  },
+  { field: 'createdAt', headerName: 'Creada en', width: 300 },
+];
 
 const OrdersPage = () => {
+  const { data, error } = useSWR<IOrder[]>('/api/admin/orders');
+
+  if (!data && !error) return <></>;
+
+  const rows = data!.map((order) => ({
+    id: order._id,
+    email: (order.user as IUser).email,
+    name: (order.user as IUser).name,
+    total: order.total,
+    isPaid: order.isPaid,
+    noProducts: order.numberOfItems,
+    createdAt: order.createdAt,
+  }));
+
   return (
     <AdminLayout
       title="Ordenes"
       subtitle="Mantenimiento de ordenes"
       icon={<ConfirmationNumberOutlined />}
     >
-      Hola mundo
+      <Grid container className="fadeIn">
+        <Grid item xs={12} sx={{ height: 650, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSizeOptions={[10]}
+            initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 10,
+                },
+              },
+            }}
+          />
+        </Grid>
+      </Grid>
     </AdminLayout>
   );
 };
