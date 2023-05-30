f7186f764d7a6a628261edf09577a8ecfb59612c
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-05-29 22:56:17 -0500

    Change user role

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import { PeopleOutline } from '@mui/icons-material';
 import { AdminLayout } from '@/components/layouts';
-import { Grid } from '@mui/material';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Grid, MenuItem, Select } from '@mui/material';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import useSWR from 'swr';
 import { IUser } from '@/interfaces';
+import { tesloApi } from '@/api';
 
 const UsersPage = () => {
   const { data, error } = useSWR<IUser[]>('/api/admin/users');
 
   if (!data && !error) return <></>;
 
+  const onRoleUpdated = async (userId: string, newRole: string) => {
+    try {
+      await tesloApi.put(`admin/users`, { userId, role: newRole });
+    } catch (error) {
+      console.log(error);
+      alert('Error al actualizar el rol del usuario');
+    }
+  };
+
   const columns: GridColDef[] = [
     { field: 'email', headerName: 'Correo', width: 250 },
     { field: 'name', headerName: 'Nombre completo', width: 300 },
-    { field: 'role', headerName: 'Rol', width: 300 },
+    {
+      field: 'role',
+      headerName: 'Rol',
+      width: 300,
+      //@ts-ignore
+      renderCell: ({ row }: GridValueGetterParams) => {
+        return (
+          <Select
+            value={row.role}
+            label="rol"
+            sx={{ width: '300px' }}
+            onChange={({ target }) => onRoleUpdated(row.id, target.value)}
+          >
+            <MenuItem value="admin">Admin</MenuItem>
+            <MenuItem value="client">Client</MenuItem>
+            <MenuItem value="super-user">Super-user</MenuItem>
+            <MenuItem value="SEO">SEO</MenuItem>
+          </Select>
+        );
+      },
+    },
   ];
 
   const rows = data!.map((user) => ({
