83a4de9f41ad31593394339a44fdc2ae8ad5064b
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-07-31 20:39:17 -0500

    useWindowSize hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,23 @@ import { ShopLayout } from '@/components/layouts';
 import { ProductList } from '@/components/products';
 import { useProducts } from '@/hooks';
 import { FullScreenLoading } from '@/components/ui';
+import { useWindowSize } from '@/hooks/useWindowSize';
 
 export default function Home() {
   const { products, isLoading } = useProducts('/products');
+  const { height, width } = useWindowSize();
+
+  console.log('height', height, 'width', width);
 
   return (
     <ShopLayout
       title={'Teslo-Shop - Home'}
       pageDescription={'Encuentra los mejores productos de Teslo aquí'}
     >
-      <Typography variant="h1" component="h1">
+      <Typography variant='h1' component='h1'>
         Tienda
       </Typography>
-      <Typography variant="h2" sx={{ mb: 1 }}>
+      <Typography variant='h2' sx={{ mb: 1 }}>
         Todos los productos
       </Typography>
 
